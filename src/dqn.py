d8c3425740103d41afc7c08c39f7ed51f6b0ad0c
ilyas <oulkaddailyas@gmail.com>
2025-02-13 02:40:43 +0100

    weights copying, rewards monitoring

--- src/dqn.py
+++ src/dqn.py
@@ -46,42 +46,61 @@ def train(
     main,
     optimizer,
     env_name="CartPole-v1",
-    episodes=10000,
+    steps=100_000,
+    capacity=100_000,
     epsilon=1,
-    decay=0.001,
-    min_eps=0.1,
-    batch_size=2,
-    gamma=0.01,
+    decay=0.0001,
+    min_eps=0.01,
+    batch_size=32,
+    gamma=0.99,
     device="mps",
 ):
     env = gym.make(env_name, render_mode="human")  # Use "human" for visualization
-    N = 100_000
-    replay_buffer = ReplayBuffer(N)
+    replay_buffer = ReplayBuffer(capacity)
+    min_experiences = 1000
+    update_frequency = 1000
+    total_steps = 0
 
-    for _ in range(episodes):
+    while total_steps < steps:
         obs, info = env.reset()
         current_state = obs
         done = False
         truncated = False
+        episode_reward = 0
 
         while not (done or truncated):
+            total_steps += 1
+            if total_steps > steps:
+                break
+
+            if total_steps % update_frequency == 0:
+                print("Target weights are being updated")
+                target.load_state_dict(main.state_dict())
+
             if torch.rand(1).item() > epsilon:
                 input = torch.tensor(current_state).to(device)
-                action = main(input).argmax()
+                action = main(input).argmax().item()
 
             else:
                 action = env.action_space.sample()  # Take random action
 
             obs, reward, done, truncated, info = env.step(action)
+            episode_reward += reward  # Accumulate episode reward
             replay_buffer.add(current_state, action, reward, obs, done)
+            current_state = obs
 
-            if len(replay_buffer) > batch_size:
+            if len(replay_buffer) > min_experiences:
                 current_states, actions, rewards, next_states, dones = (
                     replay_buffer.sample(batch_size)
                 )
 
+                current_states = current_states.to(device)
+                actions = actions.to(device)
                 current_q_values = main(current_states).gather(1, actions)
                 with torch.no_grad():
+                    next_states = next_states.to(device)
+                    rewards = rewards.to(device)
+                    dones = dones.to(device)
                     targets = rewards + gamma * target(next_states).max(dim=1)[0] * (
                         1 - dones
                     )
@@ -92,6 +111,7 @@ def train(
                 optimizer.step()
 
         epsilon = max(min_eps, epsilon - decay)
+        print(f"Epsilon value : {epsilon}, Cumulated reward : {episode_reward}")
 
     env.close()
 
