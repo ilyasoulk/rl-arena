9aaa24ca17e549b86b986ad40bf6ba34de461d52
ilyas <oulkaddailyas@gmail.com>
2025-02-15 01:05:39 +0100

    update hyperparameters, add update frequency

--- launch_cartpole_dqn.sh
+++ launch_cartpole_dqn.sh
@@ -3,10 +3,11 @@
 python src/dqn.py \
 	--env_name CartPole-v1 \
 	--hidden_dim 128 \
-	--steps 100000 \
-	--capacity 100000 \
+	--steps 50_000 \
+	--capacity 10_000 \
 	--epsilon 1 \
-	--decay 0.0001 \
+	--update_frequency 1000 \
+	--decay 0.001 \
 	--min_eps 0.01 \
 	--batch_size 32 \
 	--gamma 0.99 \
