26a73cb7dad1011cc483f604bb006bc75f9d9c05
ilyas <oulkaddailyas@gmail.com>
2025-02-16 22:16:57 +0100

    added ALE, pong experiment

---
+++ launch_pong_dqn.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+python src/dqn.py \
+	--env_name ALE/Pong-v5 \
+	--hidden_dim 128 \
+	--steps 500_000 \
+	--capacity 100_000 \
+	--epsilon 1 \
+	--update_frequency 1000 \
+	--decay 0.00001 \
+	--min_eps 0.01 \
+	--batch_size 64 \
+	--gamma 0.99 \
+	--lr 0.0005 \
+	--output_dir models \
+	--solved_threshold 21
