c98d9c1ee7304f39a306cff455eb15e025b7c0a4
ilyas <oulkaddailyas@gmail.com>
2025-02-16 22:17:59 +0100

    fix convnet for pong

--- src/models.py
+++ src/models.py
@@ -18,29 +18,30 @@ class MLP(nn.Module):
 class ConvNet(nn.Module):
     def __init__(self, observation_space, hidden_dim, action_space):
         super().__init__()
-        in_channels = observation_space[-1]
+        in_channels = observation_space[-1]  # 1 for grayscale images
+
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels, 16, kernel_size=8, stride=4),  # Output: 23x23x16
+            nn.Conv2d(in_channels, 32, kernel_size=8, stride=4),  # Output: 20x20x32
             nn.ReLU(),
-            nn.Conv2d(16, 32, kernel_size=4, stride=2),  # Output: 10x10x32
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),  # Output: 9x9x64
             nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=3, stride=1),  # Output: 8x8x32
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),  # Output: 7x7x64
             nn.ReLU(),
-            nn.Flatten(),  # Output: 2048 (8*8*32)
-            nn.Linear(2048, hidden_dim),
+            nn.Flatten(),  # Output: 3136 (7*7*64)
+            nn.Linear(3136, 512),
             nn.ReLU(),
         )
 
-        self.head = nn.Linear(hidden_dim, action_space)
+        self.head = nn.Linear(512, action_space)  # Output: action_space (Q-values)
 
     def forward(self, x):
         if len(x.shape) == 3:
-            x = x.unsqueeze(dim=0)
+            x = x.unsqueeze(dim=0)  # Add batch dim
 
         if x.max() > 1:
             x = x / 255.0
 
         x = x.permute(0, 3, 1, 2)  # B, C, H, W
-        features = self.features
+        features = self.features(x)
         actions = self.head(features)
         return actions
