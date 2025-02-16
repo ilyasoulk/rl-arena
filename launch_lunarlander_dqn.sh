f257ac940dabcdb1619b6d70c5ae33b60a5452b1
ilyas <oulkaddailyas@gmail.com>
2025-02-16 03:28:48 +0100

    update lunar lander hyperparameters

--- launch_lunarlander_dqn.sh
+++ launch_lunarlander_dqn.sh
@@ -3,14 +3,14 @@
 python src/dqn.py \
 	--env_name LunarLander-v3 \
 	--hidden_dim 128 \
-	--steps 50_000 \
-	--capacity 10_000 \
+	--steps 500_000 \
+	--capacity 100_000 \
 	--epsilon 1 \
 	--update_frequency 1000 \
-	--decay 0.001 \
+	--decay 0.00001 \
 	--min_eps 0.01 \
-	--batch_size 32 \
+	--batch_size 64 \
 	--gamma 0.99 \
-	--lr 0.001 \
+	--lr 0.0005 \
 	--output_dir models \
 	--solved_threshold 200
